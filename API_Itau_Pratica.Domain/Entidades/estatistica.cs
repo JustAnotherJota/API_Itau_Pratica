4b5eb7bbf556ed1757329b2a76886bfb1c275940
Jota <jpbernardino9@gmail.com>
2025-05-27 11:19:48 -0300

    Aplicando_DDD

---
+++ API_Itau_Pratica.Domain/Entidades/estatistica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Itau_Pratica.Domain.Entidades
+{
+    public class estatistica
+    {
+        public int count;
+        public double sum;
+        public double avg;
+        public double min;
+        public double max;
+    }
+}
---
+++ API_Itau_Pratica.Domain/Entidades/transacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Itau_Pratica.Domain.Entidades
+{
+    public class transacao
+    {
+        public double valor;
+        public DateTime dataHora;
+    }
+}
